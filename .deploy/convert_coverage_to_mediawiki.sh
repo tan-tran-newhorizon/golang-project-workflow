5e4770dd851b16494df4f6d0274c717bd7438930
tan-tran-newhorizon <tan.tran@newhorizon-tech.vn>
2024-05-23 17:46:40 +0700

    fix: fix test coverage report

--- .deploy/convert_coverage_to_mediawiki.sh
+++ .deploy/convert_coverage_to_mediawiki.sh
@@ -1,42 +1,41 @@
 #!/bin/bash
 
-# Check if go tool cover is installed
-if ! command -v go &> /dev/null; then
-    echo "Go tool could not be found. Please install Go to use this script."
+# Check if input file provided as argument
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <input_file>"
     exit 1
 fi
 
-# Check if the input file is provided
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 path_to_coverprofile"
+input_file="$1"
+
+# Check if input file exists
+if [ ! -f "$input_file" ]; then
+    echo "Input file '$input_file' not found."
     exit 1
 fi
 
-INPUT_FILE="$1"
+# Read input from file
+input=$(< "$input_file")
 
-# Generate coverage summary using go tool cover
-COVERAGE_SUMMARY=$(go tool cover -func="$INPUT_FILE")
+title="== Test Coverage Summary ==\n"
 
-# Convert to MediaWiki format using awk
-echo "== Test Coverage Summary =="
+# Output string template
+output_template="{| class=\"wikitable\"
+! File !! Line !! Function !! Coverage
+|-
+"
 
-echo "$COVERAGE_SUMMARY" | awk '
-BEGIN {
-    print "{| class=\"wikitable\""
-    print "! File !! Function !! Coverage"
-    total_line = ""
-}
-/total:/ {
-    total_line = " '''Total Coverage''': " $NF
-    next
-}
-NR > 1 {
-    printf("|- \n")
-    printf("| %s || %s || %s \n", $1, $2, $NF)
-}
-END {
-    print "|}"
-    print ""
-    print total_line
-}
-'
+# Parse input and format output
+formatted_output=""
+while IFS= read -r line; do
+    file=$(echo "$line" | awk -F ':' '{print $1}')
+    line_number=$(echo "$line" | awk -F ':' '{print $2}')
+    function=$(echo "$line" | awk '{print $(NF-1)}')
+    coverage=$(echo "$line" | awk '{print $NF}')
+    formatted_output+="| $file || $line_number || $function || $coverage\n|-\n"
+done <<< "$(echo "$input" | grep -v "total")"
+
+total_coverage=$(echo "$input" | grep "total" | awk '{print $(NF)}')
+
+# Output the formatted result
+echo -e  $title"* '''Total Coverage''': $total_coverage\n""$output_template" "$formatted_output""|}"
